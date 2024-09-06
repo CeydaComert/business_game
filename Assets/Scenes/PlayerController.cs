835c2f0fc4594775d45fe6ab77d2b6a2e6116fd0
Ceyda <ceydacomert1003@gmail.com>
2024-09-06 23:31:33 +0300

    Player Controller

--- Assets/Scenes/PlayerController.cs
+++ Assets/Scenes/PlayerController.cs
@@ -1,18 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   [Header("Setting")] [SerializeField] private float moveSpeed;
+   [Header("Control")] [SerializeField] private float slidSpeed;
+   private Vector3 clickScreenPosition;
+   private Vector3 clickPlayerPosition;
+   private void Start()
+   {
+      
+   }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private void Update()
+   {
+      MoveSpeedForward();
+      ManageControl();
+   }
+
+   private void MoveSpeedForward()
+   {
+      transform.position += Vector3.forward * Time.deltaTime * moveSpeed;
+   }
+
+   private void ManageControl()
+   {
+      if (Input.GetMouseButtonDown(0))
+      {
+         clickScreenPosition = Input.mousePosition;
+         clickPlayerPosition = transform.position;
+      }
+      else if (Input.GetMouseButton(0))
+      {
+         float xScreenDifference = Input.mousePosition.x - clickScreenPosition.x;
+
+         xScreenDifference /= Screen.width;
+         xScreenDifference *= slidSpeed;
+
+         Vector3 pos = transform.position;
+         pos.x = clickPlayerPosition.x + xScreenDifference;
+         transform.position = pos;
+         
+        // transform.position = clickPlayerPosition + Vector3.right * xScreenDifference;
+      }
+   }
 }
